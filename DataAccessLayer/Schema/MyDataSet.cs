f5aed3e8bcef99eb92cdfaf4ef71b9f8eb6d87fd
Poy Chang <poy.chang@seec.com.tw>
2017-11-29 17:18:33 +0800

    建立資料庫及資料表的模型

---
+++ DataAccessLayer/Schema/MyDataSet.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Schema
+{
+    /// <summary>資料表模型</summary>
+    public class MyDataSet
+    {
+        /// <summary>識別碼</summary>
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        /// <summary>說明</summary>
+        public string Description { get; set; }
+    }
+}
---
+++ DataAccessLayer/Schema/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Schema
+{
+    /// <summary>資料庫模型</summary>
+    public class MyDbContext : DbContext
+    {
+        /// <summary>建構式</summary>
+        /// <param name="options">設定 MyDbContext 的選項</param>
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        /// <summary>資料表</summary>
+        public DbSet<MyDataSet> MyDataSet { get; set; }
+    }
+}
