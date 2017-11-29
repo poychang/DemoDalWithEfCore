a642880a8665e741081caa6be80f47fabfdda05b
Poy Chang <poy.chang@seec.com.tw>
2017-11-29 17:46:18 +0800

    建立 DesignTimeDbContextFactory，用於開發時期進行資料庫 Migrations 操作

---
+++ ServiceLayer/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using DataAccessLayer.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace ServiceLayer
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<MyDbContext>
+    {
+        public MyDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var options = new DbContextOptionsBuilder<MyDbContext>()
+                .UseSqlite(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("ServiceLayer"))
+                .Options;
+
+            return new MyDbContext(options);
+        }
+    }
+}
