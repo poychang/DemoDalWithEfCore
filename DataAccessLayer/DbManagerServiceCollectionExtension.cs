2565b92b03f89b77426503df87751f473ad39125
Poy Chang <poy.chang@seec.com.tw>
2017-11-30 10:28:14 +0800

    重構，抽出 DbManager 父類別並設定使用 InMemory 資料庫

--- DataAccessLayer/DbManagerServiceCollectionExtension.cs
+++ DataAccessLayer/DbManagerServiceCollectionExtension.cs
@@ -4,9 +4,30 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace DataAccessLayer
 {
-    /// <summary>設定 SqliteManager 服務的擴展方法</summary>
+    /// <summary>設定 DbManager 服務的擴展方法</summary>
     public static class DbManagerServiceCollectionExtension
     {
+        /// <summary>注入 DbManager 服務(使用 InMemory Database)至指定的 IServiceCollection</summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+        /// <param name="setupAction">The middleware configuration options.</param>
+        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+        public static IServiceCollection AddDbManager(this IServiceCollection services, Action<DbManagerOptions> setupAction)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (setupAction != null)
+            {
+                services.Configure(setupAction);
+            }
+
+            services.TryAddSingleton<IDbManager, DbManager>();
+
+            return services;
+        }
+
         /// <summary>注入 SqliteManager 服務至指定的 IServiceCollection</summary>
         /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
         /// <param name="setupAction">The middleware configuration options.</param>
